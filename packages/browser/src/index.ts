10c0b0fd4b8630d85c08b598a48ec2ad395f901b
Kamil Ogórek <kamil.ogorek@gmail.com>
2021-03-10 15:02:47 +0100

    [v7] Inline browser exports file

--- packages/browser/src/index.ts
+++ packages/browser/src/index.ts
@@ -1,4 +1,27 @@
-export * from './exports';
+export { Scope } from '@sentry/scope';
+export { getHubFromCarrier, getCurrentHub, Hub, makeMain } from '@sentry/hub';
+export { configureScope, startTransaction, withScope } from '@sentry/minimal';
+export { SDK_VERSION } from '@sentry/core';
+export {
+  addBreadcrumb,
+  addGlobalEventProcessor,
+  captureEvent,
+  captureException,
+  captureMessage,
+  close,
+  flush,
+  lastEventId,
+  setContext,
+  setExtra,
+  setExtras,
+  setTag,
+  setTags,
+  setUser,
+} from '@sentry/minimal';
+
+export { BrowserClient, BrowserOptions } from './client';
+export { defaultIntegrations, forceLoad, init, onLoad, showReportDialog, wrap } from './sdk';
+export { SDK_NAME } from './version';
 
 import { getGlobalObject } from '@sentry/utils';
 
