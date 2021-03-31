4fe39573dbb50c4a09a8d7d0356d28d6a5792ec0
Kamil Ogórek <kamil.ogorek@gmail.com>
2021-03-31 12:30:28 +0200

    [v7] Remove unnecessary async/await from transport-fetch

--- packages/transport-fetch/src/index.ts
+++ packages/transport-fetch/src/index.ts
@@ -15,16 +15,16 @@ export class FetchTransport extends BaseTransport implements Transport {
       requestOptions.credentials = this._options.credentials as RequestCredentials;
     }
 
-    return fetch(this._dsn.getEnvelopeEndpoint(), requestOptions).then(async response => {
-      return {
-        body: await response.text(),
+    return fetch(this._dsn.getEnvelopeEndpoint(), requestOptions).then(response => {
+      return response.text().then(body => ({
+        body,
         headers: {
           'x-sentry-rate-limits': response.headers.get('X-Sentry-Rate-Limits'),
           'retry-after': response.headers.get('Retry-After'),
         },
         reason: response.statusText,
         statusCode: response.status,
-      };
+      }));
     });
   }
 }
